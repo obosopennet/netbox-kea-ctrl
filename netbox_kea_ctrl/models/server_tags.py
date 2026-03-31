e84139216922e9a59f790394bc9709ade66b3638
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-03-31 09:46:47 +0200

    Add KeaServerTag model for server tagging

---
+++ netbox_kea_ctrl/models/server_tags.py
@@ -0,0 +1,31 @@
+from django.db import models
+from netbox.models import NetBoxModel
+
+from netbox_kea_ctrl.choices import FAMILY_CHOICES, FAMILY_IPV4
+
+
+class KeaServerTag(NetBoxModel):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True)
+    enabled = models.BooleanField(default=True)
+    family = models.CharField(max_length=10, choices=FAMILY_CHOICES, default=FAMILY_IPV4)
+
+    ha_group = models.ForeignKey(
+        to="netbox_kea_ctrl.KeaHAGroup",
+        on_delete=models.PROTECT,
+        related_name="server_tags",
+        null=True,
+        blank=True,
+    )
+
+    configuration_backend_target = models.CharField(
+        max_length=255,
+        blank=True,
+        help_text="Identifier used by the publisher/backend adapter",
+    )
+
+    class Meta:
+        ordering = ("name",)
+
+    def __str__(self):
+        return self.name
