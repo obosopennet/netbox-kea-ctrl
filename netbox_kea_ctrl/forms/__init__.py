96faa573409c28cbed22c8d15079a4a87b602948
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-03-31 09:48:58 +0200

    Initialize forms module with form imports

---
+++ netbox_kea_ctrl/forms/__init__.py
@@ -0,0 +1,11 @@
+from .ha import KeaHAGroupForm
+from .server_tags import KeaServerTagForm
+from .shared_networks import KeaSharedNetworkForm
+from .actions import MaintenanceActionForm
+
+__all__ = (
+    "KeaHAGroupForm",
+    "KeaServerTagForm",
+    "KeaSharedNetworkForm",
+    "MaintenanceActionForm",
+)
