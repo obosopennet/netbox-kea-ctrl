722fd2d6d8f5d7335d8940f4ebe1c7ce3a90381a
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-03-31 09:46:02 +0200

    Initialize models module with imports and exports

---
+++ netbox_kea_ctrl/models/__init__.py
@@ -0,0 +1,11 @@
+from .ha import KeaHAGroup
+from .server_tags import KeaServerTag
+from .shared_networks import KeaSharedNetwork
+from .publish_jobs import KeaPublishJob
+
+__all__ = (
+    "KeaHAGroup",
+    "KeaServerTag",
+    "KeaSharedNetwork",
+    "KeaPublishJob",
+)
