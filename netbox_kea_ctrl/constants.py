0cb6a19cbd6aaf50b6863d35d431a9db22602df8
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-03-31 09:44:33 +0200

    Add constants for KEA configuration and validation

--- netbox_kea_ctrl/constants.py
+++ netbox_kea_ctrl/constants.py
@@ -1,1 +1,12 @@
+KEA_MANAGED_TAG = "kea-managed"
 
+CF_KEA_SHARED_NETWORK = "kea_shared_network"
+CF_KEA_SERVER_TAG = "kea_server_tag"
+CF_KEA_POOL_RANGE = "kea_pool_range"
+CF_KEA_OPTION_DATA = "kea_option_data"
+
+VALIDATION_IGNORED = "ignored"
+VALIDATION_VALID = "valid"
+VALIDATION_VALID_STANDALONE = "valid-standalone"
+VALIDATION_WARNING = "warning"
+VALIDATION_ERROR = "error"
