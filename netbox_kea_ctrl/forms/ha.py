8f8dec3f8509d9f3e0f96e0be9ad2f8eaaf671cb
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-03-31 10:19:45 +0200

    Update ha.py

--- netbox_kea_ctrl/forms/ha.py
+++ netbox_kea_ctrl/forms/ha.py
@@ -1,11 +1,18 @@
-from .ha import KeaHAGroupForm
-from .server_tags import KeaServerTagForm
-from .shared_networks import KeaSharedNetworkForm
-from .actions import MaintenanceActionForm
+from netbox.forms import NetBoxModelForm
 
-__all__ = (
-    "KeaHAGroupForm",
-    "KeaServerTagForm",
-    "KeaSharedNetworkForm",
-    "MaintenanceActionForm",
-)
+from netbox_kea_ctrl.models import KeaHAGroup
+
+
+class KeaHAGroupForm(NetBoxModelForm):
+    class Meta:
+        model = KeaHAGroup
+        fields = (
+            "name",
+            "description",
+            "mode",
+            "enabled",
+            "primary_name",
+            "secondary_name",
+            "primary_control_agent_url",
+            "secondary_control_agent_url",
+        )
