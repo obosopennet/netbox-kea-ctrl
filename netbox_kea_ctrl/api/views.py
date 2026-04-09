e047e92a4068f5d3c99a6b304d2472677e894d55
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-04-09 07:27:32 +0200

    Update views.py

--- netbox_kea_ctrl/api/views.py
+++ netbox_kea_ctrl/api/views.py
@@ -1,8 +1,41 @@
 from netbox.api.viewsets import NetBoxModelViewSet
-from netbox_kea_ctrl.models import KeaServer
-from .serializers import KeaServerSerializer
+
+from netbox_kea_ctrl.models import (
+    KeaHAGroup,
+    KeaPublishJob,
+    KeaServer,
+    KeaServerTag,
+    KeaSharedNetwork,
+)
+from .serializers import (
+    KeaHAGroupSerializer,
+    KeaPublishJobSerializer,
+    KeaServerSerializer,
+    KeaServerTagSerializer,
+    KeaSharedNetworkSerializer,
+)
+
+
+class KeaHAGroupViewSet(NetBoxModelViewSet):
+    queryset = KeaHAGroup.objects.all()
+    serializer_class = KeaHAGroupSerializer
 
 
 class KeaServerViewSet(NetBoxModelViewSet):
     queryset = KeaServer.objects.all()
     serializer_class = KeaServerSerializer
+
+
+class KeaServerTagViewSet(NetBoxModelViewSet):
+    queryset = KeaServerTag.objects.all()
+    serializer_class = KeaServerTagSerializer
+
+
+class KeaSharedNetworkViewSet(NetBoxModelViewSet):
+    queryset = KeaSharedNetwork.objects.all()
+    serializer_class = KeaSharedNetworkSerializer
+
+
+class KeaPublishJobViewSet(NetBoxModelViewSet):
+    queryset = KeaPublishJob.objects.all()
+    serializer_class = KeaPublishJobSerializer
