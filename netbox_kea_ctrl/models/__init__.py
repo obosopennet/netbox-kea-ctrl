12f7817306fa3e6a2d9e23b7a023259be1f472ee
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-04-09 17:47:29 +0200

    Update __init__.py

--- netbox_kea_ctrl/models/__init__.py
+++ netbox_kea_ctrl/models/__init__.py
@@ -3,6 +3,7 @@ from .server_tags import KeaServerTag
 from .shared_networks import KeaSharedNetwork
 from .publish_jobs import KeaPublishJob
 from .servers import KeaServer
+from .pools import KeaPrefixPool
 
 __all__ = (
     "KeaHAGroup",
@@ -10,4 +11,5 @@ __all__ = (
     "KeaSharedNetwork",
     "KeaPublishJob",
     "KeaServer",
+    "KeaPrefixPool",
 )
