ef964e02add4775122f0d0f947fbd02305ddcb8b
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-04-09 07:27:21 +0200

    Update serializers.py

--- netbox_kea_ctrl/api/serializers.py
+++ netbox_kea_ctrl/api/serializers.py
@@ -1,8 +1,39 @@
 from netbox.api.serializers import NetBoxModelSerializer
-from netbox_kea_ctrl.models import KeaServer
+
+from netbox_kea_ctrl.models import (
+    KeaHAGroup,
+    KeaPublishJob,
+    KeaServer,
+    KeaServerTag,
+    KeaSharedNetwork,
+)
+
+
+class KeaHAGroupSerializer(NetBoxModelSerializer):
+    class Meta:
+        model = KeaHAGroup
+        fields = "__all__"
 
 
 class KeaServerSerializer(NetBoxModelSerializer):
     class Meta:
         model = KeaServer
+        fields = "__all__"
+
+
+class KeaServerTagSerializer(NetBoxModelSerializer):
+    class Meta:
+        model = KeaServerTag
+        fields = "__all__"
+
+
+class KeaSharedNetworkSerializer(NetBoxModelSerializer):
+    class Meta:
+        model = KeaSharedNetwork
+        fields = "__all__"
+
+
+class KeaPublishJobSerializer(NetBoxModelSerializer):
+    class Meta:
+        model = KeaPublishJob
         fields = "__all__"
