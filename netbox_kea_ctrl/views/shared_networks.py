888db726d1019ed9a1f3d3ebce6bd690e44114a1
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-04-09 08:45:31 +0200

    Update shared_networks.py

--- netbox_kea_ctrl/views/shared_networks.py
+++ netbox_kea_ctrl/views/shared_networks.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from netbox_kea_ctrl.forms import KeaSharedNetworkForm
 from netbox_kea_ctrl.models import KeaSharedNetwork
@@ -10,13 +10,33 @@ class KeaSharedNetworkListView(ListView):
     template_name = "netbox_kea_ctrl/sharednetwork_list.html"
     context_object_name = "object_list"
 
+    def get_queryset(self):
+        return (
+            KeaSharedNetwork.objects.all()
+            .select_related("server_tag", "ha_group")
+            .prefetch_related("manual_servers")
+            .order_by("name")
+        )
+
 
 class KeaSharedNetworkView(DetailView):
     model = KeaSharedNetwork
     template_name = "netbox_kea_ctrl/sharednetwork.html"
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["publish_targets"] = self.object.get_publish_targets()
+        return context
 
-class KeaSharedNetworkEditView(CreateView, UpdateView):
+
+class KeaSharedNetworkCreateView(CreateView):
+    model = KeaSharedNetwork
+    form_class = KeaSharedNetworkForm
+    template_name = "netbox_kea_ctrl/object_edit.html"
+    success_url = reverse_lazy("plugins:netbox_kea_ctrl:keasharednetwork_list")
+
+
+class KeaSharedNetworkEditView(UpdateView):
     model = KeaSharedNetwork
     form_class = KeaSharedNetworkForm
     template_name = "netbox_kea_ctrl/object_edit.html"
