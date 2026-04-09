9b7c222d33018703364001ec94996e9850029607
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-04-09 08:01:35 +0200

    Update dashboard.py

--- netbox_kea_ctrl/views/dashboard.py
+++ netbox_kea_ctrl/views/dashboard.py
@@ -1,6 +1,12 @@
 from django.views.generic import TemplateView
 
-from netbox_kea_ctrl.models import KeaHAGroup, KeaPublishJob, KeaServerTag, KeaSharedNetwork
+from netbox_kea_ctrl.models import (
+    KeaHAGroup,
+    KeaPublishJob,
+    KeaServer,
+    KeaServerTag,
+    KeaSharedNetwork,
+)
 
 
 class DashboardView(TemplateView):
@@ -8,8 +14,24 @@ class DashboardView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["shared_network_count"] = KeaSharedNetwork.objects.count()
-        context["server_tag_count"] = KeaServerTag.objects.count()
-        context["ha_group_count"] = KeaHAGroup.objects.count()
-        context["recent_jobs"] = KeaPublishJob.objects.all()[:10]
+
+        servers = KeaServer.objects.select_related("ha_group").all()
+        ha_groups = KeaHAGroup.objects.all()
+        server_tags = KeaServerTag.objects.all()
+        shared_networks = KeaSharedNetwork.objects.all()
+        publish_jobs = KeaPublishJob.objects.order_by("-created")[:10]
+
+        context["server_count"] = servers.count()
+        context["enabled_server_count"] = servers.filter(enabled=True).count()
+        context["server_error_count"] = servers.exclude(last_error="").count()
+        context["discovered_server_count"] = servers.exclude(last_seen__isnull=True).count()
+
+        context["ha_group_count"] = ha_groups.count()
+        context["server_tag_count"] = server_tags.count()
+        context["shared_network_count"] = shared_networks.count()
+
+        context["ha_groups"] = ha_groups.prefetch_related("servers")
+        context["servers_with_errors"] = servers.exclude(last_error="")[:10]
+        context["recent_publish_jobs"] = publish_jobs
+
         return context
