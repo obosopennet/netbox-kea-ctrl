46d2cf3fbee7bde0a88c1e9653e6619692f2713c
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-04-09 07:27:45 +0200

    Update urls.py

--- netbox_kea_ctrl/api/urls.py
+++ netbox_kea_ctrl/api/urls.py
@@ -1,7 +1,18 @@
 from netbox.api.routers import NetBoxRouter
-from .views import KeaServerViewSet
+
+from .views import (
+    KeaHAGroupViewSet,
+    KeaPublishJobViewSet,
+    KeaServerTagViewSet,
+    KeaServerViewSet,
+    KeaSharedNetworkViewSet,
+)
 
 router = NetBoxRouter()
+router.register("ha-groups", KeaHAGroupViewSet)
 router.register("servers", KeaServerViewSet)
+router.register("server-tags", KeaServerTagViewSet)
+router.register("shared-networks", KeaSharedNetworkViewSet)
+router.register("publish-jobs", KeaPublishJobViewSet)
 
 urlpatterns = router.urls
