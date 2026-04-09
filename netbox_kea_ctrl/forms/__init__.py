ff06c57b0c0d8e1970ed58d2e0ddd1794b0dcd6d
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-04-09 09:14:48 +0200

    Update __init__.py

--- netbox_kea_ctrl/forms/__init__.py
+++ netbox_kea_ctrl/forms/__init__.py
@@ -1,13 +1,14 @@
 from .ha import KeaHAGroupForm
 from .server_tags import KeaServerTagForm
-from .shared_networks import KeaSharedNetworkForm
+from .shared_networks import KeaSharedNetworkCreateForm, KeaSharedNetworkEditForm
 from .actions import MaintenanceActionForm
 from .servers import KeaServerForm
 
 __all__ = (
     "KeaHAGroupForm",
     "KeaServerTagForm",
-    "KeaSharedNetworkForm",
+    "KeaSharedNetworkCreateForm",
+    "KeaSharedNetworkEditForm",
     "MaintenanceActionForm",
     "KeaServerForm",
 )
