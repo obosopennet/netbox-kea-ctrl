5d2cc339de35b18ce3f2ee9849a4dc77b431da07
Tom Sverre <68464234+Tom-sverre@users.noreply.github.com>
2026-04-16 18:55:45 +0200

    Update __init__.py

--- netbox_kea_ctrl/utils/__init__.py
+++ netbox_kea_ctrl/utils/__init__.py
@@ -1,1 +1,11 @@
+from extras.models import CustomField
 
+
+def get_prefix_custom_field(name: str):
+    """
+    Return a Prefix custom field by name if it exists, otherwise None.
+    """
+    try:
+        return CustomField.objects.get(name=name, object_types__model="prefix")
+    except CustomField.DoesNotExist:
+        return None
